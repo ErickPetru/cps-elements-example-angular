f4298fbdcfacdbff012ac606da6fa7e2802d7db6
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2023-03-28 18:50:24 -0300

    feat: add CPS Elements to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+export { default as CpsButton } from '@cps-elements/web/components/button';
+export { default as CpsIcon } from '@cps-elements/web/components/icon';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cps-elements-example-angular';
+  isWaiting = false;
+  message = '';
+
+  onClick() {
+    if (this.isWaiting) return;
+
+    this.isWaiting = true;
+    setTimeout(() => {
+      this.message = `Atualizado às ${new Date().toLocaleTimeString()}.`;
+      this.isWaiting = false;
+    }, 1000);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,43 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { setBasePath } from '@cps-elements/web/utilities/base-path';
+import { registerIconLibrary } from '@cps-elements/web/utilities/icon-library';
+
+setBasePath('https://cdn.jsdelivr.net/npm/@cps-elements/web');
+
+registerIconLibrary('uil', {
+  resolver: (name) => {
+    const [_, file, mode] = name.match(/^(.*?)(-fill|-mono|-thin)?$/)!;
+    const folder =
+      { '-fill': 'solid', '-mono': 'monochrome', '-thin': 'thinline' }[mode] ??
+      'line';
+    return `https://cdn.jsdelivr.net/npm/@iconscout/unicons@4.0.5/svg/${folder}/${file}.svg`;
+  },
+  mutator: (svg) => {
+    svg.setAttribute('fill', 'currentColor');
+    svg.removeAttribute('id');
+    svg.removeAttribute('data-name');
+    svg.innerHTML = svg.innerHTML
+      .replace(
+        /class="uim-primary"/g,
+        'style="color: rgb(var(--cps-color-neutral-600)"'
+      )
+      .replace(
+        /class="uim-secondary"/g,
+        'style="color: rgb(var(--cps-color-neutral-500)"'
+      )
+      .replace(
+        /class="uim-tertiary"/g,
+        'style="color: rgb(var(--cps-color-neutral-300)"'
+      )
+      .replace(
+        /class="uim-quaternary"/g,
+        'style="color: rgb(var(--cps-color-neutral-200)"'
+      );
+  },
+});
+
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
